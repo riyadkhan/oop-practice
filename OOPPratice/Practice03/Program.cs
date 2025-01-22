b21d3eaf07b1f45ff100a1b196a7afd55c183f88
riyadkhan <dev.riyad@gmail.com>
2025-01-22 09:21:31 +0600

    added practice 03

---
+++ OOPPratice/Practice03/Program.cs
@@ -0,0 +1,12 @@
+﻿using Practice03;
+
+var car = new Vehicle("Toyota", "2020", "Sedan", 4, 5500);
+
+car.RentCar(new DateTime(2025, 01, 10), new DateTime(2025, 01, 12));
+
+bool isCarAvailable = car.IsCarAvailable(new DateTime(2025, 01, 11), new DateTime(2025, 01, 12));
+
+Console.WriteLine($"IsCarAvailable: {isCarAvailable}");
+
+
+car.RentCar(new DateTime(2025, 01, 12), new DateTime(2025, 01, 15));
---
+++ OOPPratice/Practice03/Vehicle.cs
@@ -0,0 +1,63 @@
+﻿namespace Practice03
+{
+    public class Vehicle
+    {
+        public Guid Id { get; private set; }
+        public string Brand { get; private set; }
+        public string Model { get; private set; }
+        public string Type { get; private set; }
+        public int NumberOfSeat { get; private set; }
+        public float RentAmount { get; private set; }
+        public List<DateTime> BookedDate { get; set; }
+
+        public Vehicle(string brand, string model, string type, int numberOfSeat, float rentAmount)
+        {
+            Id = Guid.NewGuid();
+            Brand = brand;
+            Model = model;
+            Type = type;
+            NumberOfSeat = numberOfSeat;
+            RentAmount = rentAmount;
+            BookedDate = new List<DateTime>();
+        }
+
+        public void RentCar(DateTime startDate, DateTime endDate)
+        {
+            if (!IsCarAvailable(startDate, endDate))
+            {
+                throw new Exception("Car is not available");
+            }
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                BookedDate.Add(date);
+            }
+        }
+
+        public void SendToMaintenance(DateTime startDate, DateTime endDate)
+        {
+            if (!IsCarAvailable(startDate, endDate))
+            {
+                throw new Exception("Car is not available");
+            }
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                BookedDate.Add(date);
+            }
+        }
+
+        public bool IsCarAvailable(DateTime startDate, DateTime endDate)
+        {
+            List<DateTime> requiredDates = new List<DateTime>();
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                requiredDates.Add(date);
+            }
+
+            if (BookedDate.Intersect(requiredDates).Count() > 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
